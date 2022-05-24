3c670ac65c3f5f2f0440b1b6337e4dea1c5abddf
AMammedova <ayselmemmedova1718@gmail.com>
2022-05-24 16:39:14 +0400

    reactchanges

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+const Header =()=>{
+    
+        return (
+            <header className="header">
+                <h1 className="header__title">
+                Görmək lazımdır
+                </h1>
+            </header>
+        );
+    }
+
+ 
+export default Header;
